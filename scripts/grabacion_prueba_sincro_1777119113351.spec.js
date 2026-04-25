4c88f3f34b37d1f523c6f0a609950d338ee5d7e0
pgalvez237 <pierre.galvez@seidor.com>
2026-04-25 07:13:37 -0500

    autobot: nuevo escenario grabado - 4/25/2026, 7:13:37 AM

---
+++ scripts/grabacion_prueba_sincro_1777119113351.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://ao1k9k5jk.accounts.ondemand.com/oauth2/authorize?client_id=2fad98f3-610b-4304-bf84-83187c448577&response_type=code&redirect_uri=https%3A%2F%2Fmedifarma-portal-qa-63shgxes.authentication.us10.hana.ondemand.com%2Flogin%2Fcallback%2Fsap.custom&state=AWwjCp12IL&code_challenge=KAUb3AZe8s7TMUPNvyTW0QpNYTuH2DcuYHfg60pCMkQ&code_challenge_method=S256&scope=email+openid+profile&nonce=giRykXNATXk7');
+  await page.getByRole('textbox', { name: 'Correo electrónico o nombre' }).click();
+  await page.getByRole('textbox', { name: 'Correo electrónico o nombre' }).click();
+  await page.getByRole('textbox', { name: 'Correo electrónico o nombre' }).fill('pierre.galvez@seidor.com');
+  await page.getByRole('textbox', { name: 'Contraseña' }).click();
+  await page.getByRole('textbox', { name: 'Contraseña' }).fill('Pavilion2371126.@');
+  await page.getByRole('button', { name: 'Continuar' }).click();
+  await page.getByRole('link', { name: 'Registro de Manufactura' }).click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('textbox', { name: 'Lote' }).click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('textbox', { name: 'Lote' }).fill('2040196');
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('textbox', { name: 'Lote' }).press('Enter');
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('textbox', { name: 'Lote' }).click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('textbox', { name: 'Lote' }).press('Enter');
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('button', { name: 'Ir' }).click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('button', { name: 'Ir' }).press('Enter');
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('cell', { name: 'FABR' }).click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().locator('td:nth-child(12)').first().click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByLabel('Opciones de selección').click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByText('1', { exact: true }).click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('button', { name: 'Confirmar' }).click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('option', { name: 'NOTAS IMPORTANTES DURANTE EL' }).click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('option', { name: 'EQUIPOS / INSTRUMENTOS /' }).click();
+  await page.locator('iframe[title="Aplicación"]').contentFrame().getByRole('option', { name: 'INSUMOS' }).click();
+  await page.getByText('PG').click();
+  await page.getByRole('button', { name: 'Perfil de Pierre Galvez' }).click();
+  await page.getByText('Salir').click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
